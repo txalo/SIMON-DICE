96aaf4c023781ac684ce2c2685504e5af576b327
txalo <txalomdq@gmail.com>
2021-06-17 11:18:05 -0300

    Funciones basicas
    Agregue funciones basicas:
    - Reproducir secuencia (recibe array con numeros y reproduce una secuencia de acuerdo a esos numeros)
    - Funcion presionarCuadro () reproduce sonido y cambia opacidad de elemento que viene por parametro.
    - Un solo listener para todos los cuadros (onclick llama a funcion presionarCuadro() )
    - agregarCiclo (), genera un numero aleatorio y lo agrega a un array secuencia.

--- js/main.js
+++ js/main.js
@@ -1,29 +1,61 @@
 
-var sounds = {
-    "boton-1": new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3"),
-    "boton-2": new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3"),
-    "boton-3": new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3"),
-    "boton-4": new Audio ("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3"),
+const sounds = {
+    "cuadro-1": new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3"),
+    "cuadro-2": new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3"),
+    "cuadro-3": new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3"),
+    "cuadro-4": new Audio ("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3"),
 };
+const secuencia = [];
 
+function presionarCuadro (cuadro){
+  sounds[cuadro.id].play();
+  cuadro.style.opacity = 1;
+  setTimeout(function(){cuadro.style.opacity = 0.75},500);
+}
 
-  const $verde = document.querySelector("#boton-1");
-  const $rojo = document.querySelector("#boton-2");
-  const $amarillo = document.querySelector("#boton-3");
-  const $azul  = document.querySelector("#boton-4");
-
-  $verde.onclick = function(){
-    sounds["boton-1"].play();
+document.querySelectorAll(".cuadro").forEach(function(element){
+  element.onclick = function(){
+    presionarCuadro(element);
   }
+});
 
-  $rojo.onclick = function(){
-    sounds["boton-2"].play();
-  }
+function reproducirSecuencia(secuencia){
+  for (let i = 0; i < secuencia.length; i++){
+    setTimeout(function(){presionarCuadro(document.querySelector("#cuadro-"+secuencia[i]))},i * 900);
+  }  
+}
 
-  $amarillo.onclick = function(){
-    sounds["boton-3"].play();
-  }
+function agregarCiclo (secuencia){
+  //let numero = ((Math.random()* 100)%4);
+  let numero = (Math.random() * (4 - 1) + 1);
+  secuencia.push(Math.round(numero));
+}
 
-  $azul.onclick = function(){
-    sounds["boton-4"].play();
-  }
+for(let i = 0; i < 5;i++){
+  agregarCiclo(secuencia);
+  console.log(secuencia);
+}
+
+reproducirSecuencia(secuencia);
+/*
+$verde.onclick = function(){
+  sounds["cuadro-1"].play();
+}
+
+$rojo.onclick = function(){
+  sounds["cuadro-2"].play();
+  $rojo.style.backgroundColor = "#f00";
+  setTimeout(function(){$rojo.style.backgroundColor = "#dc143c"},200);
+}
+
+$amarillo.onclick = function(){
+  sounds["cuadro-3"].play();
+  $amarillo.style.backgroundColor = "#ff0";
+  setTimeout(function(){$amarillo.style.backgroundColor = "#ffd700"},200);
+}
+
+$azul.onclick = function(){
+  sounds["cuadro-4"].play();
+  $azul.style.opacity = 1;
+  setTimeout(function(){$azul.style.opacity = 0.75},200);
+}*/
